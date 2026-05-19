96536f82f06ca47fb666fb752bb86b9b711755aa
njat8702 <njat8702@gmail.com>
2026-05-19 22:18:27 +0530

    create the new file

---
+++ comparison.js
@@ -0,0 +1,35 @@
+// // basic comparison operation
+// console.log(50>30)
+// console.log(50<30)
+// console.log(50>=30)
+// console.log(50<=30)
+
+// // comparison string
+// console.log("apple">"banana");
+// console.log("glowing">"glow");
+
+// // type Coercion in conversion
+// console.log("2">1);
+// console.log("01"==1);
+
+// // strict equality(===) vs loose equality(==)
+// console.log("01"==1);
+// console.log("01"===1);
+
+// // null and undefined in comparison
+// console.log(null===undefined);
+// console.log(null==undefined);
+
+// // null in mathematical comparison
+// console.log(null>0);
+// console.log(null<1);
+// console.log(null>=0);
+// console.log(null==0);
+
+//task
+console.log(3<=5);
+console.log("mango">"banana");
+console.log("2">"3");
+console.log(undefined==null);
+console.log(null===undefined);
+console.log(null<1);
