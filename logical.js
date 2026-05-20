06defbe9a1a17d4d2cc8731b33f68a5fd6d8d222
njat8702 <njat8702@gmail.com>
2026-05-20 10:04:18 +0530

    add nullish coalescing operatin logical.js

--- logical.js
+++ logical.js
@@ -21,3 +21,21 @@ if(!isStudentEligible){
 }else{
     console.log("you are eligible.")
 }
+
+// nullish coalescing
+// 1.defaultValue
+let name;
+console.log(name ?? "Nandini");
+
+// 2.value
+const fristName="Neha";
+console.log(fristName ?? "Nandini");
+
+// 3.empty string
+let Name;
+console.log(Name ?? "Nandini");
+
+// falsy values
+const a=0;
+console.log(a||1);
+console.log(a??1);
