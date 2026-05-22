c5e95c9f15e5f73735dd6df03e12c3cba3e7d429
njat8702 <njat8702@gmail.com>
2026-05-22 08:38:19 +0530

    edit aOperation.js file

---
+++ arrays/aOperation.js
@@ -0,0 +1,43 @@
+// push
+// function func(){
+//     var arr=['GFG','gfg'];
+//     arr.push("GeeksForGeeks");
+//     console.log(arr);
+// }
+// func();
+
+// function func1(){
+//     var arr=[32,345546,765];
+//     arr.push(23,46,76);
+//     console.log(arr);
+// }
+// func1();
+
+// pop
+// function func(){
+//     var arr =['GFG','gfg','Gfg','GeeksForGeeks'];
+//     var popped=arr.pop();
+//     console.log(popped);
+//     console.log(arr);
+    
+// }
+// func();
+
+// function func1(){
+//     var arr =[];
+//     var popped=arr.pop();
+//     console.log(popped);
+//     // console.log(arr);
+    
+// }
+// func1();
+
+// slice()
+function func(){
+    var arr=[23,56,87,32,75,13];
+    var new_arr = arr.slice(2);
+    console.log(arr);
+    // console.log("<br>");
+    console.log(new_arr);
+}
+func();
